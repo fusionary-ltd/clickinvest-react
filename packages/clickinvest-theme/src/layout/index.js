c681f3cd7f5bc0de15a5e44dd7ee5a19272664a3
miracle0116 <jaimew0404@gmail.com>
2022-10-28 22:50:12 +0900

    [update] ReCaptcha

--- packages/clickinvest-theme/src/layout/index.js
+++ packages/clickinvest-theme/src/layout/index.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { connect } from "frontity"
-import ReCaptchaV2 from 'react-google-recaptcha'
 import Switch from "@frontity/components/switch"
 import MuiThemeProvider from "../provider/Theme"
-
+import { GoogleReCaptchaProvider, GoogleReCaptcha } from 'react-google-recaptcha-v3';
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Home from "../pages/home/index"
@@ -14,19 +13,46 @@ import { Accessibility } from 'accessibility'
 const Root = ({ state }) => {
   const data = state.source.get(state.router.link)
   const { reCaptcha } = state.option
+
+  const [token, setToken] = useState();
+
   useEffect(() => {
     window.addEventListener('load', function () { new Accessibility(); }, false)
   }, [])
 
+  console.log(token, 'token----')
+
   return (
     <MuiThemeProvider>
-      <Header />
-      <Switch>
-        <Home when={data.isArchive} />
-        <Post when={data.isPostType} />
-      </Switch>
-      <Footer />
-      <ReCaptchaV2 sitekey={reCaptcha.siteKey} />
+      <GoogleReCaptchaProvider
+        reCaptchaKey={reCaptcha.siteKey}
+        // useRecaptchaNet="[optional_boolean_value]"
+        // useEnterprise="[optional_boolean_value]"
+        scriptProps={{
+          async: false, // optional, default to false,
+          defer: false, // optional, default to false
+          appendTo: 'head', // optional, default to "head", can be "head" or "body",
+          nonce: undefined // optional, default undefined
+        }}
+        container={{
+          parameters: {
+            badge: ['bottomleft'],
+            theme: 'dark',
+          }
+        }}
+      >
+        <Header />
+        <Switch>
+          <Home when={data.isArchive} />
+          <Post when={data.isPostType} />
+        </Switch>
+        <Footer />
+        <GoogleReCaptcha
+          onVerify={token => {
+            setToken(token);
+          }}
+        />
+      </GoogleReCaptchaProvider>
     </MuiThemeProvider>
   )
 }
