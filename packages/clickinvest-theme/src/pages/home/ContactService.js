eca68e278019edf99ca76ec85374936b37ffdfbb
miracle0116 <jaimew0404@gmail.com>
2022-11-02 21:25:15 +0900

    [update] contact data

--- packages/clickinvest-theme/src/pages/home/ContactService.js
+++ packages/clickinvest-theme/src/pages/home/ContactService.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from "frontity";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -6,10 +6,11 @@ import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import { RepairInput, RepairButton } from '../../components/styled';
+import { GoogleReCaptcha } from 'react-google-recaptcha-v3';
 
 const ContactService = ({ state }) => {
     const { theme, home } = state.option;
-
+    const [token, setToken] = useState();
     const [values, setValues] = useState({
         name: '',
         phone: '',
@@ -22,7 +23,8 @@ const ContactService = ({ state }) => {
         setValues({ ...values, [prop]: event.target.value });
     };
 
-    const sendRequest = () => {
+    const sendRequest = (e) => {
+        e.preventDefault();
         let temp = error;
         if (values.name) {
             temp = { ...temp, name: false };
@@ -58,7 +60,14 @@ const ContactService = ({ state }) => {
             temp = { ...temp, location: true };
         }
         setError(temp);
+        console.log({ ...values, reCaptcha: token, origin: location.host });
     }
+
+    useEffect(() => {
+        if (document && document.getElementsByClassName('grecaptcha-badge')[0]) {
+            document.getElementsByClassName('grecaptcha-badge')[0].style.display = 'none';
+        }
+    }, [token])
 
     return (
         <Box
@@ -71,37 +80,44 @@ const ContactService = ({ state }) => {
                 <Stack>
                     <Typography variant='h3' sx={{ mb: 2.5, fontSize: { md: '2.4rem', xs: '1.5rem' }, fontWeight: 700 }}>{home.contact.title}</Typography>
                     <Typography sx={{ mb: 2.5, fontSize: '1rem' }}>{home.contact.description}</Typography>
-                    <Grid container>
-                        <Grid item sm={12 / 5} xs={12}>
-                            <Stack sx={{ mb: { md: 0, xs: 2 } }}>
-                                <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="Full Name" name="full-name" variant="outlined" onChange={handleValue('name')} />
-                                {error.name && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
-                            </Stack>
+                    <form onSubmit={(e) => sendRequest(e)} >
+                        <Grid container>
+                            <Grid item sm={12 / 5} xs={12}>
+                                <Stack sx={{ mb: { md: 0, xs: 2 } }}>
+                                    <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="Full Name" name="full-name" variant="outlined" onChange={handleValue('name')} />
+                                    {error.name && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
+                                </Stack>
+                            </Grid>
+                            <Grid item sm={12 / 5} xs={12}>
+                                <Stack sx={{ mb: { md: 0, xs: 2 } }}>
+                                    <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="Phone Number" name="tel-323" type="tel" variant="outlined" onChange={handleValue('phone')} />
+                                    {error.phone && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
+                                </Stack>
+                            </Grid>
+                            <Grid item sm={12 / 5} xs={12}>
+                                <Stack sx={{ mb: { md: 0, xs: 2 } }}>
+                                    <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="Email Address" name="your-email" type="email" variant="outlined" onChange={handleValue('email')} />
+                                    {error.email && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
+                                </Stack>
+                            </Grid>
+                            <Grid item sm={12 / 5} xs={12}>
+                                <Stack sx={{ mb: { md: 0, xs: 2 } }}>
+                                    <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="City / Location" name="city-location" variant="outlined" onChange={handleValue('location')} />
+                                    {error.location && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
+                                </Stack>
+                            </Grid>
+                            <Grid item sm={12 / 5} xs={12}>
+                                <RepairButton sx={{ width: '100%', bgcolor: theme.secondary, '&:hover': { bgcolor: theme.secondary } }} type='submit'>Submit</RepairButton>
+                            </Grid>
                         </Grid>
-                        <Grid item sm={12 / 5} xs={12}>
-                            <Stack sx={{ mb: { md: 0, xs: 2 } }}>
-                                <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="Phone Number" name="tel-323" type="tel" variant="outlined" onChange={handleValue('phone')} />
-                                {error.phone && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
-                            </Stack>
-                        </Grid>
-                        <Grid item sm={12 / 5} xs={12}>
-                            <Stack sx={{ mb: { md: 0, xs: 2 } }}>
-                                <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="Email Address" name="your-email" type="email" variant="outlined" onChange={handleValue('email')} />
-                                {error.email && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
-                            </Stack>
-                        </Grid>
-                        <Grid item sm={12 / 5} xs={12}>
-                            <Stack sx={{ mb: { md: 0, xs: 2 } }}>
-                                <RepairInput sx={{ mr: { sm: 1, xs: 0 } }} placeholder="City / Location" name="city-location" variant="outlined" onChange={handleValue('location')} />
-                                {error.location && <Typography sx={{ color: theme.warning }}>Please enter the correct value.</Typography>}
-                            </Stack>
-                        </Grid>
-                        <Grid item sm={12 / 5} xs={12}>
-                            <RepairButton sx={{ width: '100%', bgcolor: theme.secondary, '&:hover': { bgcolor: theme.secondary } }} onClick={() => sendRequest()}>Submit</RepairButton>
-                        </Grid>
-                    </Grid>
+                    </form>
                 </Stack>
             </Container>
+            <GoogleReCaptcha
+                onVerify={token => {
+                    setToken(token);
+                }}
+            />
         </Box>
     );
 }
